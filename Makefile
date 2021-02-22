0f2b314766d6a5ec6f0c9a85f2868002ee1598a3
bionade24 <o.roesler@oscloud.info>
2021-02-22 22:58:45 +0100

    Add setup.py

--- Makefile
+++ Makefile
@@ -4,3 +4,7 @@ all:
 	pyuic5 -o rm102/mainwindow.py mainwindow.ui
 remove:
 	rm102/mainwindow.py
+install:
+	python3 -m pip install .
+uninstall:
+	python3 -m pip uninstall registermachine102
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "registermachine102",
+    version = "0.1",
+    description = "Better implementation of rm101, a fake assembler for educational purposes.",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/bionade24/registermachine102",
+    author = "Oskar Roesler (bionade24)",
+    author_email = "o.roesler@oscloud.info",
+    license = "GPLv3",
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+        # Pick your license as you wish
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires = ["argparse", "docker-compose"],
+    packages = ["rm102"],
+    scripts = ["main.py"]
+)
